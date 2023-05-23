d8ffbb73c6cd62402a0fca895b108320463fb8d1
Ilya Lyakhov <ilja.sarasti@mail.ru>
2023-05-23 22:21:44 +0400

    Added mongoDB driver, not finished

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,16 @@
 package user
+
+import (
+	"context"
+	"rest-api-project/pkg/logging"
+)
+
+type Service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context, dto CreateUserDTO) (User, error) {
+	// TODO next
+	return User{}, nil
+}
